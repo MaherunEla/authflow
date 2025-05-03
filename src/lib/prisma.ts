e5cdf1f9a640c24139c1ec7ec0747f0aae513bce
Maherun Nessa <meherunela2002@gmail.com>
2025-05-03 09:39:29 +0600

    change in lib prisma file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,9 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
+let prisma: PrismaClient;
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({
     // @ts-expect-error - suppress TS error for internal field
     __internal: {
       engine: {
@@ -14,8 +11,46 @@ export const prisma =
       },
     },
   });
+} else {
+  // In dev mode, reuse the client to avoid exhausting connections
+  const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+  };
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient({
+      // @ts-expect-error - suppress TS error for internal field
+      __internal: {
+        engine: {
+          statement_cache: false,
+        },
+      },
+    });
+  }
+
+  prisma = globalForPrisma.prisma;
+}
+
+export { prisma };
+
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     // @ts-expect-error - suppress TS error for internal field
+//     __internal: {
+//       engine: {
+//         statement_cache: false,
+//       },
+//     },
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // import { PrismaClient } from "@prisma/client";
 
